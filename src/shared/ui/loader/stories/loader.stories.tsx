85d40d0586fa997620b93d5f3d8f63f1a47b15f2
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2021-10-17 02:13:43 +0300

    feat: loader like dog

--- src/shared/ui/loader/stories/loader.stories.tsx
+++ src/shared/ui/loader/stories/loader.stories.tsx
@@ -7,4 +7,14 @@ export default {
   title: 'Loader',
 };
 
-export const Default = () => <Loader />;
+export const Default = () => (
+  <div
+    style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    }}>
+    <Loader />
+  </div>
+);
