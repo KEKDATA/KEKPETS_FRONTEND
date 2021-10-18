c108f9654db8dda85d3963f435755de4b97b4d7a
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2021-10-18 17:38:46 +0300

    fix: build

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/images/favicon.svg',
       },
     },
     'gatsby-transformer-remark',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
+import { useGate } from 'effector-react';
 import { PageProps } from 'gatsby';
 import React from 'react';
+import { searchModel } from 'shared/models/search';
 
+import { Pagination } from 'features/pagination';
+import { Results } from 'features/results';
 import { ScrollToTop } from 'features/scroll_to_top';
-
-import { SearchPage } from 'pages/search';
+import { SearchSettings } from 'features/search_settings';
 
 import makeFakeServer from '../requests/fake';
 import '../styles.css';
 
 makeFakeServer();
 
-const Search: React.FC<PageProps> = () => (
-  <main>
-    <SearchPage />
-    <ScrollToTop scrollPositionThreshold={700} />
-  </main>
-);
+const Search: React.FC<PageProps> = () => {
+  useGate(searchModel.SearchGate);
+
+  return (
+    <main>
+      <SearchSettings />
+      <Results />
+      <Pagination />
+      <ScrollToTop scrollPositionThreshold={700} />
+    </main>
+  );
+};
 
 export default Search;
