7918b351de6d1ca1ff356d92a3f73dbf9fd179c5
sinptr <zoneofatombomb@gmail.com>
2021-10-18 19:14:20 +0300

    feat(docker): add Dockerfile and docker-compose.yml

--- src/requests/config.ts
+++ src/requests/config.ts
@@ -1,6 +1,6 @@
 import ky from 'ky';
 
-export const prefixUrl = 'http://localhost:3000/api/v1';
+export const prefixUrl = process.env.API_URL || 'http://localhost:3000/api/v1';
 
 export const apiConfig = ky.create({
   prefixUrl,
