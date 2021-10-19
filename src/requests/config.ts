41cddc4cd973571195e8e31dc774de4d124b2ea3
sinptr <zoneofatombomb@gmail.com>
2021-10-19 14:40:36 +0300

    feat(deploy): fix config

--- src/requests/config.ts
+++ src/requests/config.ts
@@ -2,9 +2,6 @@ import ky from 'ky';
 
 export const prefixUrl = process.env.API_URL || 'http://localhost:3000/api/v1';
 
-// TODO: remove after testing
-console.log('prefixUrl', prefixUrl);
-
 export const apiConfig = ky.create({
   prefixUrl,
   timeout: 60000,
